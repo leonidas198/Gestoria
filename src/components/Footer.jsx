da424430e9bfc4abc4e378427d4b906921982fd6
[quique] <[enriquechavez1982@hotmail.com]>
2025-06-16 11:27:21 -0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,23 @@
 export const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2025 Gestoría Digital AR - Todos los derechos reservados -  Este sitio participa en el Programa de Afiliados de Amazon, un programa de publicidad para afiliados diseñado para ofrecer un medio a sitios web para obtener comisiones por publicidad, publicitando e incluyendo enlaces a Amazon.com. Como Asociado de Amazon, ganamos con las compras que califican, sin costo adicional para ti.</p>
-      
-      <div className="social-links">
+  <p>
+    &copy; 2025 Gestoría Digital AR - Todos los derechos reservados -  
+    Este sitio participa en el Programa de Afiliados de Amazon, un programa de publicidad para afiliados diseñado para ofrecer un medio a sitios web para obtener comisiones por publicidad, publicitando e incluyendo enlaces a Amazon.com. Como Asociado de Amazon, ganamos con las compras que califican, sin costo adicional para ti.
+  </p>
 
-        
-        <a href="https://www.facebook.com" target="_blank" rel="noreferrer">Facebook</a>
-        <a href="https://www.instagram.com" target="_blank" rel="noreferrer">Instagram</a>
-        <a href="mailto:contacto@gestoria.com">Email</a>
-      </div>
-    </footer>
+  <p>
+    ¿Buscás una web que se vea bien y se posicione en Google? Visitá mi  
+    <a href="https://my-portfolio-jrop.vercel.app/" target="_blank" rel="noopener"> Mi portfolio</a>
+
+  </p>
+
+  <div className="social-links">
+    <a href="https://www.facebook.com" target="_blank" rel="noreferrer">Facebook</a>
+    <a href="https://www.instagram.com" target="_blank" rel="noreferrer">Instagram</a>
+    <a href="mailto:contacto@gestoria.com">Email</a>
+  </div>
+</footer>
+
   )
 }
